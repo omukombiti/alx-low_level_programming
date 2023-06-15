174f3fd0e5d919376b154eb01817e3143f017a31
omukombiti <mwirigiomukombiti@gmail.com>
2023-06-15 12:53:47 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,36 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+/**
+
+ * main - Entry point
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	int i;
+
+	int charcount;
+
+
+	charcount = 0;
+
+
+	for (i = 0; quote[i]; i++)
+
+		charcount++;
+
+
+	fwrite(quote, sizeof(char), charcount, stderr);
+
+	return (1);
+
+}
